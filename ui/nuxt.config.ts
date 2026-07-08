deecd820adb8bf6b2bfd04f2372630da59519a5a
Nelson <oonelsoncodes@gmail.com>
2026-07-08 23:37:04 +0100

    updated the server files

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -1,9 +1,9 @@
 export default defineNuxtConfig({
-  runtimeConfig: {
-    public: {
-      apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:5000'
-    }
-  },
-  modules: [],
-  css: ['~/assets/css/main.css']
-})
+	runtimeConfig: {
+		public: {
+			apiUrl: process.env.NUXT_PUBLIC_API_URL || "http://localhost:5000",
+		},
+	},
+	modules: [],
+	// css: ['~/assets/css/main.css']
+});
